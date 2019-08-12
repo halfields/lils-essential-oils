a39617551c69d3507769052f5b810f0d12630852
halfields <hfields697@aol.com>
2019-08-11 23:17:57 -0500

    add 'webdriver' gem to gemfile, draw  routes for root, auth/facebook/callback, categories, products, carts, line_items, users, and admin:products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#welcome'
+  get 'auth/facebook/callback' => 'sessions#create'
+
+  resources :categories do 
+  	resources :products, only: [:index, :show]
+  end
+
+  resources :carts do 
+  	resources :line_items, only: [:index, :show]
+  end
+
+  resources :orders
+  resources :line_items
+  resources :users, only: [:index, :new, :create, :destroy]
+
+  scope :admin do 
+  	resources :products, only: [:new, :create]
+  end
 end
