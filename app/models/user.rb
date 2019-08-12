38e8bf198c06819b767f73efc954d16dcffaa5b0
halfields <hfields697@aol.com>
2019-08-11 23:44:19 -0500

    add validations and 'has_secure_password' to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+	before_save {self.email = email.downcase} #ensures program can hanle mixed case email even if database adapter cannot handle mixed case indexes
+	validates :name, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, 
+										uniqueness: {case_sensitive: false}
+	validates :password, presence: true, length: {minimum: 8}
+	has_secure_password
 end
