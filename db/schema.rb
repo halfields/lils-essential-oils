58aea1cec4f3519ba24e855493064397bbcd246d
halfields <hfields697@aol.com>
2019-09-01 21:48:07 -0500

    stub out a session model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_035645) do
+ActiveRecord::Schema.define(version: 2019_08_28_153632) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 2019_08_12_035645) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.string "image_url"
     t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
