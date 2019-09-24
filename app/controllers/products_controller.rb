8b6dee976af09d4c62f5f4ce685d843767448026
halfields <hfields697@aol.com>
2019-09-24 17:30:54 -0500

    add index and show actions to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
 class ProductsController < ApplicationController 
 #	paginates_per 3
 	max_paginates_per 3
+
   def index
-  	@products = Product.order('product.name asc').page(params[:page])
+		@category = Category.find_by(id: params[:category_id])
+		@products = @category.products.order('product.name asc').page(params[:page])
+		render '/categories/show'
+
+  	# @products = Product.order('product.name asc').page(params[:page])
   end
 
   def show
+  	@category = Category.find_by(id: params[:category_id])
+  	@product = @category.products.find_by(id: params[:id])
   end
 
   def new
